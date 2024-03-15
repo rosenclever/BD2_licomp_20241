b6abca04450466b387bd21f26007e00a4e9c7085
rosenclever <rosenclever@gmail.com>
2024-03-14 21:05:39 -0300

    aula do dia 14/03

--- comandosDDL.sql
+++ comandosDDL.sql
@@ -12,3 +12,39 @@ use exemplodb;
 
 -- excluindo um banco de dados
 drop database exemplodb;
+
+-- criar o bancoDB
+create database bancoDB;
+
+-- acessar o bancoDB
+use bancoDB;
+
+-- criando a tabela bancos
+create table bancos(
+    numero int not null,
+    nome varchar(100) not null,
+    constraint bancos_pk primary key(numero)
+);
+
+-- criar a tabela agencias
+create table agencias(
+    numero varchar(100) not null,
+    nome varchar(100) not null,
+    telefone int,
+    banco int not null,
+    constraint agencias_pk primary key(numero)
+);
+
+--criar a tabela clientes
+create table clientes(
+    id int not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    constraint clientes_pk primary key(id)
+);
+
+-- criar a coluna uf do tipo char com tamanho 2 e valor padrão 'RJ' na tabela clientes
+alter table clientes
+add uf char(2) default 'RJ';
+
+-- 
